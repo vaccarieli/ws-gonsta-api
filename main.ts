a9a0bb0b709925845cdfab9ce4dddd95ef9c7aea
elio gonzalez <vacarieli@gmail.com>
2023-02-09 17:44:40 -0500

    made sure to await functions

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
 // custom modules
 import config from "./src/config";
-import { startSock } from "./src/launcher";
-import { userSocks } from "./src/makeWaSocket";
-import { app } from "./src/message";
-import { event_handler } from "./src/handler";
-import { store_data } from "./src/data";
+import {startSock} from "./src/launcher";
+import {userSocks} from "./src/makeWaSocket";
+import {app} from "./src/message";
+import {event_handler} from "./src/handler";
+import {store_data} from "./src/data";
 
 const startApp = async () => {
-  store_data();
-  await startSock();
+    store_data();
+    await startSock();
 
-  for (const user in userSocks) await event_handler(userSocks, user);
+    for (const user in userSocks) await event_handler(userSocks, user);
 
-  app.listen(config.APP_PORT, () => {
-    console.log(`App listening on ${config.APP_IP}:${config.APP_PORT}`);
-  });
+    app.listen(config.APP_PORT, () => {
+        console.log(`App listening on ${config.APP_IP}:${config.APP_PORT}`);
+    });
 };
 
-startApp();
+(async () => {
+    await startApp();
+})();
