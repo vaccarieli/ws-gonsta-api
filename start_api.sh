33dcb17f34366fe2b1304496e87c7c982f86644f
elio gonzalez <vacarieli@gmail.com>
2023-02-10 10:23:58 -0500

    removed files with irelevant information

--- start_api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a new tmux session
-tmux new-session -d -s api
-
-# Send a command to the tmux session
-tmux send-keys -t api "yarn --cwd /home/vaccarieli/ws-gonsta-api/ start" C-m
--- start_bot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a new tmux session
-tmux new-session -d -s bot
-
-# Send the command to the tmux session
-tmux send-keys -t bot "python3 /home/vaccarieli/ws-gonsta-api/bot/bot.py" C-m
